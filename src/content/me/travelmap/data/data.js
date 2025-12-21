261807cf18700ff51f5c37acf7b35b62d7fb13d6
Huilin-Li <LHLLIHUILIN@outlook.com>
2025-12-21 11:35:11 +0800

    update map

--- src/content/me/travelmap/data/data.js
+++ src/content/me/travelmap/data/data.js
@@ -5,24 +5,29 @@ var data = {
       "cities": [
         {
           "name": "BeiJing",
+          "description": "",
           "coordinates": [39.9042, 116.4074]
         },
         {
           "name": "TianJing",
+          "description": "",
           "coordinates": [39.0851, 117.1994]
         },
         {
           "name": "ShiJiaZhuang",
+          "description": "",
           "coordinates": [38.0428, 114.5143]
         },
         {
           "name": "ShangHai",
+          "description": "",
           "coordinates": [31.2304, 121.4737]
         },
-                {
+        {
           "name": "WuZhen",
+          "description": "",
           "coordinates": [30.7426, 120.4878]
-        },
+        }
       ]
     },
     {
@@ -30,18 +35,22 @@ var data = {
       "cities": [
         {
           "name": "Nice",
+          "description": "",
           "coordinates": [43.7102, 7.2620]
         },
         {
           "name": "Cannes",
+          "description": "",
           "coordinates": [43.5539, 7.0170]
         },
         {
           "name": "Menton",
+          "description": "",
           "coordinates": [43.7745, 7.4975]
         },
         {
           "name": "Antibes",
+          "description": "",
           "coordinates": [43.5804, 7.1251]
         }
       ]
@@ -51,6 +60,7 @@ var data = {
       "cities": [
         {
           "name": "Monaco",
+          "description": "",
           "coordinates": [43.7384, 7.4246]
         }
       ]
