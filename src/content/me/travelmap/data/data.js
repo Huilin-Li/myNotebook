4859df1636ffa79a5a74b25f81d4fd055e2b410a
Huilin-Li <LHLLIHUILIN@outlook.com>
2025-12-21 12:57:33 +0800

    update world map

--- src/content/me/travelmap/data/data.js
+++ src/content/me/travelmap/data/data.js
@@ -6,7 +6,7 @@ var data = {
         {
           "name": "BeiJing",
           "description": "",
-          "coordinates": [39.9042, 116.4074]
+          "coordinates": [39.90243091506673, 116.40362913496195]
         },
         {
           "name": "TianJing",
@@ -36,12 +36,12 @@ var data = {
         {
           "name": "Nice",
           "description": "",
-          "coordinates": [43.7102, 7.2620]
+          "coordinates": [43.708486272227134, 7.263282724011568]
         },
         {
           "name": "Cannes",
           "description": "",
-          "coordinates": [43.5539, 7.0170]
+          "coordinates": [43.55554718041898, 7.015086553960624]
         },
         {
           "name": "Menton",
@@ -52,6 +52,11 @@ var data = {
           "name": "Antibes",
           "description": "",
           "coordinates": [43.5804, 7.1251]
+        },
+        {
+          "name": "Paris",
+          "description": "",
+          "coordinates": [48.85611493927924, 2.348061974170208]
         }
       ]
     },
@@ -61,7 +66,62 @@ var data = {
         {
           "name": "Monaco",
           "description": "",
-          "coordinates": [43.7384, 7.4246]
+          "coordinates": [43.73720684936715, 7.420978628459574]
+        }
+      ]
+    },
+    {
+      "id": "NLD",
+      "cities": [
+        {
+          "name": "Groningen",
+          "description": "",
+          "coordinates": [53.219099162566515, 6.565692815802476]
+        },
+        {
+          "name": "Amsterdam",
+          "description": "",
+          "coordinates": [52.369732243769526, 4.9067050218523365]
+        },
+        {
+          "name": "Leiden",
+          "description": "",
+          "coordinates": [52.16268225348795, 4.477721043638955]
+        },
+        {
+          "name": "Delft",
+          "description": "",
+          "coordinates": [52.012208287074266, 4.357002460070548]
+        },
+        {
+          "name": "Rotterdam",
+          "description": "",
+          "coordinates": [51.92478156770275, 4.4788098263012595]
+        },
+        {
+          "name": "Utrecht",
+          "description": "",
+          "coordinates": [52.08922109238915, 5.121971646880335]
+        },
+        {
+          "name": "Eindhoven",
+          "description": "",
+          "coordinates": [51.422581230769765, 5.464041179533855]
+        },
+        {
+          "name": "Maastricht",
+          "description": "",
+          "coordinates": [50.850853892556216, 5.693009469532131]
+        }
+      ]
+    },
+    {
+      "id": "BEL",
+      "cities": [
+        {
+          "name": "Bruxelles",
+          "description": "",
+          "coordinates": [50.848829738914986, 4.359451534574198]
         }
       ]
     }
